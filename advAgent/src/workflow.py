60d3c6d0120a716f53fa8e508f2c5abd29184b0e
Eddiejab <200442868+EddieJabraouti@users.noreply.github.com>
2025-12-24 17:11:18 -0500

    created appropriate graph nodes + edges for effective communication between agents in the graph

--- advAgent/src/workflow.py
+++ advAgent/src/workflow.py
@@ -13,9 +13,17 @@ class Workflow:
         self.prompts = DeveloperToolsPrompts()
         self.workflow = self._build_workflow()
 
-
+    #initialize all agent nodes
     def _build_workflow(self):
-        pass
+        graph = StateGraph(ResearchState)
+        graph.add_node("extract_tools", self._extract_tools_step)
+        graph.add_node("research", self._research_step)
+        graph.add_node("analyze",  self._analyze_step)
+        graph.set_entry_point("extract_tools")
+        graph.add_edge("extract_tools", "research")
+        graph.add_edge("research", "analyze")
+        graph.add_edge("analyze", END)
+        return graph.compile()
 
     #first step in graph
     def _extract_tools_step(self, state: ResearchState) -> Dict[str, Any]:
